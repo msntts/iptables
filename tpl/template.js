b9896043dcaa1105a367a7b1a417996999b471c3
msntts <terzonome-github@yahoo.co.jp>
2021-10-25 23:30:13 +0900

    iptablesのルール削除ボタンの表示位置調整

--- tpl/template.js
+++ tpl/template.js
@@ -29,7 +29,7 @@ var tpl = {
             '<td class="rowright py-2" id="pkts' + index + '"></td>' +
             '<td class="rowright py-2" id="bytes' + index + '"></td>' +
             '<td class="row py-2" id="rule' + index + '"><span class="edittext text-lg">' + text + '<i class="fas fa-edit mx-2" onclick="parser.editRule(' + index + ')"></i></spawn></td>' +
-            (index ? '<td class="rowcenter"><a href="#" onclick="return rules.remove(' + index + ');" title="Delete"><svg class="octicon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path fill-rule="evenodd" d="M3.72 3.72a.75.75 0 011.06 0L8 6.94l3.22-3.22a.75.75 0 111.06 1.06L9.06 8l3.22 3.22a.75.75 0 11-1.06 1.06L8 9.06l-3.22 3.22a.75.75 0 01-1.06-1.06L6.94 8 3.72 4.78a.75.75 0 010-1.06z"></path></svg></a>' + "</td>" : '<td class="row"></td>') +
+            (index ? '<td class="rowcenter text-center"><a href="#" onclick="return rules.remove(' + index + ');" title="Delete"><svg class="octicon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path fill-rule="evenodd" d="M3.72 3.72a.75.75 0 011.06 0L8 6.94l3.22-3.22a.75.75 0 111.06 1.06L9.06 8l3.22 3.22a.75.75 0 11-1.06 1.06L8 9.06l-3.22 3.22a.75.75 0 01-1.06-1.06L6.94 8 3.72 4.78a.75.75 0 010-1.06z"></path></svg></a>' + "</td>" : '<td class="row"></td>') +
             "</tr>";
     },
 };
